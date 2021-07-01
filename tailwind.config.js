526723d3f187e6838ba071d2b835b26c5e969732
Lakshya Kumar <lakshya.kumar_cs19@gla.ac.in>
2021-07-01 18:37:32 +0530

    configured tailwind config file

---
+++ tailwind.config.js
@@ -0,0 +1,46 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      primary: {
+        100: "#f1f8f6",
+        200: "#d4e9e2",
+        300: "#008248",
+        400: "#1e3932",
+      },
+      secondary: "#f3f1e7",
+      gray: colors.gray,
+      white: colors.white,
+      black: colors.black,
+    },
+    fontFamily: {
+      sans: "'Helvetica Neue',Helvetica,Arial,sans-serif",
+    },
+    extend: {
+      backgroundImage: {
+        hero: "url('./img/hero-img.webp')",
+      },
+      fontSize: {
+        13: "0.8125rem",
+        "2.5xl": "1.75rem",
+        19: "1.188rem",
+      },
+      padding: {
+        1.75: "0.4375rem",
+      },
+      margin: {
+        18: "4.5rem",
+      },
+      letterSpacing: {
+        lessTight: "-0.009em",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/aspect-ratio")],
+};
