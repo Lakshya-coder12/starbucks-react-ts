dafbb651827bcc47e630c5c9fcf03d916f303266
Lakshya Kumar <lakshya.kumar_cs19@gla.ac.in>
2021-07-01 20:06:08 +0530

    added HeroSection component in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./index.css";
 import Navbar from "./Navbar";
 import Header from "./Header";
+import HeroSection from "./HeroSection";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Header />
+      <HeroSection />
     </div>
   );
 }
