b8b59dcb3ec2c2fec1c17c2acfc8d7a7b395d3c2
Lakshya Kumar <lakshya.kumar_cs19@gla.ac.in>
2021-07-02 23:35:24 +0530

    improved underlined link to add svg for external links

--- src/UnderlinedLink.tsx
+++ src/UnderlinedLink.tsx
@@ -5,24 +5,39 @@ interface Props extends AnchorHTMLAttributes<HTMLAnchorElement> {
   theme?: "green";
   children: string;
   className?: string;
+  externalLink: boolean;
 }
 
 const UnderlinedLink: React.FC<Props> = ({
   theme,
   children,
   className,
+  externalLink,
   ...rest
 }) => {
   let themeClass = theme === "green" ? " text-primary-300 " : " ";
+  let svgClass = externalLink === true ? "inline" : "hidden";
   return (
-    <a
-      {...rest}
-      className={
-        "flex-shrink-0 underline hover:no-underline" + themeClass + className
-      }
-    >
-      {children}
-    </a>
+    <span>
+      <a
+        {...rest}
+        className={
+          "flex-shrink-0 underline hover:no-underline" + themeClass + className
+        }
+      >
+        {children}
+        <svg
+          viewBox="0 0 24 24"
+          preserveAspectRatio="xMidYMid meet"
+          aria-hidden="true"
+          focusable="false"
+          className={"w-6 h-6 " + svgClass}
+          fill="#008248"
+        >
+          <path d="M14.75 18H7.875C6.84 18 6 17.16 6 16.125V9.25c0-1.036.84-1.875 1.875-1.875h5.11c.276 0 .5.224.5.5s-.224.5-.5.5h-5.11c-.483 0-.875.392-.875.875v6.875c0 .483.392.875.875.875h6.875c.483 0 .875-.392.875-.875V11c0-.276.224-.5.5-.5s.5.224.5.5v5.125c0 1.035-.84 1.875-1.875 1.875zM14 6.5c0-.276.224-.5.5-.5h3c.276 0 .5.224.5.5v3c0 .276-.224.5-.5.5s-.5-.224-.5-.5V7.72l-4.66 4.74c-.19.196-.507.2-.704.005-.197-.193-.2-.51-.006-.707L16.307 7H14.5c-.276 0-.5-.224-.5-.5z"></path>
+        </svg>
+      </a>
+    </span>
   );
 };
 
