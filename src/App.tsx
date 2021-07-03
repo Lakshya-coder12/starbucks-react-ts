be354deaf1ff53fcc8ca457a9e130615e2bc65c6
Lakshya Kumar <lakshya.kumar_cs19@gla.ac.in>
2021-07-03 12:21:39 +0530

    added StarRewards component in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import HeroSection from "./HeroSection";
 import GettingStartedSection from "./GettingStartedSection";
 import FavoritesSection from "./FavoritesSection";
 import EndlessExtrasSection from "./EndlessExtrasSection";
+import StarRewards from "./StarRewards";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <GettingStartedSection />
       <FavoritesSection />
       <EndlessExtrasSection />
+      <StarRewards />
     </div>
   );
 }
