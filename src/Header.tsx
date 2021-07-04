504dc7d9c2b5ad23714d0deed1cd61e55fdcc3ad
Lakshya Kumar <lakshya.kumar_cs19@gla.ac.in>
2021-07-04 23:03:04 +0530

    added media queries to Header component

--- src/Header.tsx
+++ src/Header.tsx
@@ -5,9 +5,15 @@ interface Props {}
 
 const Header: React.FC<Props> = (props) => {
   return (
-    <div className="sticky top-0 z-10 flex items-center justify-between px-4 py-2 bg-primary-400">
-      <p className="font-bold text-white text-13">STARBUCKS&reg; REWARDS</p>
-      <RoundedLink href="https://starbucks.app.link/VLa2I3inh9" theme="white">
+    <div className="sticky top-0 z-10 flex items-center justify-between h-full px-4 py-3.75 md:top-22 lg:top-26 bg-primary-400">
+      <p className="pl-32 font-bold tracking-wider text-white text-13">
+        STARBUCKS&reg; REWARDS
+      </p>
+      <RoundedLink
+        className="md:hidden"
+        href="https://starbucks.app.link/VLa2I3inh9"
+        theme="white"
+      >
         Join in the app
       </RoundedLink>
     </div>
