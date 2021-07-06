6fdaea644bbf61107c292034b467bec54f2545d0
Lakshya Kumar <lakshya.kumar_cs19@gla.ac.in>
2021-07-06 15:28:39 +0530

    added CSS grid for responsiveness on large screen size

--- src/EndlessExtrasSection.tsx
+++ src/EndlessExtrasSection.tsx
@@ -10,19 +10,21 @@ interface Props {}
 
 const EndlessExtrasSection: React.FC<Props> = (props) => {
   return (
-    <div className="px-4 mb-10 mt-18">
+    <div className="px-4 mb-10 md:px-6 mt-18 md:my-32">
       <H2 className="text-center">Endless Extras</H2>
-      <p className="pt-4 text-sm text-center">
+      <p className="max-w-xl pt-4 ml-auto mr-auto text-sm text-center md:text-base">
         Joining Starbucks&reg; Rewards means unlocking access to exclusive
         benefits. Say hello to easy ordering, tasty Rewards and—yes, free
         coffee.
       </p>
-      <div className="pt-12">
+      <div className="pt-12 md:flex md:mx-11 lg:mx-16">
         <StepImageComponent
           heading="Fun freebies"
           src={fun_freebies}
           alt="coffee and burger"
           ImgClass=" h-28 w-32 "
+          className="md:mr-6"
+          applyMediaQueries={true}
         >
           <p>
             Not only can you earn free coffee, look forward to a birthday treat
@@ -39,6 +41,8 @@ const EndlessExtrasSection: React.FC<Props> = (props) => {
           src={phone}
           alt="phone"
           ImgClass=" h-28 w-32 "
+          className="md:mx-6"
+          applyMediaQueries={true}
         >
           <p>
             Enjoy the convenience of in-store, curbside or drive-thru pickup at
@@ -55,6 +59,8 @@ const EndlessExtrasSection: React.FC<Props> = (props) => {
           src={smoothie}
           alt="smoothie"
           ImgClass=" h-28 w-32 "
+          className="md:ml-6"
+          applyMediaQueries={true}
         >
           <p>
             Earn Stars even quicker with Bonus Star challenges, Double Star Days
